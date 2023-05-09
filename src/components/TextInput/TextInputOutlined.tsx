0ad3c2a9d335df613ddee6134ac5605ba9de5338
Naveen <naveen.eluvathingal@experionglobal.com>
2023-05-09 09:54:29 +0530

    Label color same like outlinecolor

--- src/components/TextInput/TextInputOutlined.tsx
+++ src/components/TextInput/TextInputOutlined.tsx
@@ -329,6 +329,7 @@ const TextInputOutlined = ({
               {...labelProps}
               labelBackground={LabelBackground}
               maxFontSizeMultiplier={rest.maxFontSizeMultiplier}
+              placeholderColor={outlineColor ?? null}
             />
           ) : null}
           {render?.({
